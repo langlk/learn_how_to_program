72b7d2ae4d8d74241b767b7296d92863cfe3bf93
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-14 09:39:49 -0800

    Add CRUD for courses.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,46 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      flash[:notice] = "Course updated!"
+      redirect_to course_path(@course)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.destroy
+    flash[:notice] = "Course '#{@course.name}' has been deleted."
+    redirect_to courses_path
+  end
+private
+  def course_params
+    params.require(:course).permit(:name)
+  end
 end
