f2412012a1d6dd4d8501f29e3cca32697bfe5566
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-14 13:48:55 -0800

    Add previous and next section links to navbar.

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,4 +3,24 @@ class Section < ActiveRecord::Base
   validates :order, uniqueness: { scope: :course, message: 'already taken in this course'}
   belongs_to :course
   has_many :lessons
+
+  def next
+    sections = self.course.sections.order(:order)
+    sections.each do |section|
+      if section.order > self.order
+        return section
+      end
+    end
+    return nil
+  end
+
+  def previous
+    sections = self.course.sections.order(order: :desc)
+    sections.each do |section|
+      if section.order < self.order
+        return section
+      end
+    end
+    return nil
+  end
 end
