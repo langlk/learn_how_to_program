6dd70a5291c16fe5e57a0a2d452a1d33a1387ab9
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-14 13:42:54 -0800

    Add validation for section order uniqueness

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,6 @@
 class Section < ActiveRecord::Base
   validates :name, :order, :course, presence: true
+  validates :order, uniqueness: { scope: :course, message: 'already taken in this course'}
   belongs_to :course
   has_many :lessons
 end
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -6,4 +6,19 @@ describe Section do
   it { should belong_to :course }
   it { should validate_presence_of :course }
   it { should have_many :lessons }
+
+  it "should validate uniqueness of order" do
+    course = Course.create(name: "Test")
+    section1 = course.sections.create(name: "Test", order: 1)
+    section2 = course.sections.new(name: "Test2", order: 1)
+    expect(section2.save).to eq(false)
+  end
+
+  it "should vaidate uniqueness of order within course" do
+    course = Course.create(name: "Test")
+    course2 = Course.create(name: "Test2")
+    section1 = course.sections.create(name: "Test", order: 1)
+    section2 = course2.sections.new(name: "Test2", order: 1)
+    expect(section2.save).to eq(true) 
+  end
 end
