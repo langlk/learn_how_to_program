4be0fdb6867aa4bba4410399ad27924cb7bfdc67
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-14 09:16:44 -0800

    Add bootstrap and homepage view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'courses#index'
+
+  resources :courses do
+    resources :sections
+  end
+
+  resources :sections do
+    resources :lessons
+  end
 end
