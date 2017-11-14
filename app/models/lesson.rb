b3de30b48e9999934494322f3939c4db2e69c894
Kelsey Langlois <kels.langlois@gmail.com>
2017-11-14 11:50:13 -0800

    Add next and previous lessons methods and nav.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,26 @@
 class Lesson < ActiveRecord::Base
   validates :name, :content, :order, :section, presence: true
   belongs_to :section
+
+  def next
+    lessons = self.section.lessons.order(:order)
+    lessons.each do |lesson|
+      puts lesson.order
+      if lesson.order > self.order
+        return lesson
+      end
+    end
+    return nil
+  end
+
+  def previous
+    lessons = self.section.lessons.order(order: :desc)
+    lessons.each do |lesson|
+      puts lesson.order
+      if lesson.order < self.order
+        return lesson
+      end
+    end
+    return nil
+  end
 end
